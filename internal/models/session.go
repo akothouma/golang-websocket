81ef0bffb1f91e662ebb831538ce333617047904
Cliff Oyoh Omollo <oyoocliff471@gmail.com>
2025-02-17 18:19:44 +0300

    Add CreateSession function that creates a nes session for the user in the database by first deleting all session of the user before creating one

--- internal/models/session.go
+++ internal/models/session.go
@@ -1,10 +1,33 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
 
+	"github.com/google/uuid"
+)
 
-type Session struct{
-	ID string
-	UserID int
+type Session struct {
+	ID        string
+	UserID    int
 	ExpiresAt time.Time
+}
+
+
+func CreateSession(userID int) (string, error) {
+	querry := `DELETE FROM Sessions WHERE user_id=?`
+	_, err := DB.Exec(querry, userID)
+	if err != nil {
+		return "", fmt.Errorf("failed to delete existing sessions: %w", err)
+	}
+	SessionID := uuid.New().String()
+	expiresAt := time.Now().Add(24 * time.Hour)
+
+	query := "INSERT INTO Sessions(id, user_id, expires_at) VALUES(?, ?, ?)"
+	_, err = DB.Exec(query, SessionID, userID, expiresAt)
+	if err != nil {
+		return "", fmt.Errorf("failed to insert session: %w", err)
+	}
+
+	return SessionID, nil
 }
