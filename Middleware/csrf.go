64b99cfdbacf9dc5e2ae70f32e25300a27568eb7
Cliff Oyoh Omollo <oyoocliff471@gmail.com>
2025-02-17 18:11:14 +0300

    Add CSRFMiddleware that checks if csrf protection exists in the session cookie if not it adds and pass the token through the request context

---
+++ Middleware/csrf.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func CSRFMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("csrf_token")
+		var csrfToken string
+
+		if err != nil || cookie.Value == "" {
+			csrfToken = uuid.New().String()
+
+			http.SetCookie(w, &http.Cookie{
+				Name:     "csrf_token",
+				Value:    csrfToken,
+				Path:     "/",
+				HttpOnly: true,
+				Secure:   false,                          
+				Expires:  time.Now().Add(24 * time.Hour), 
+			})
+		} else {
+			csrfToken = cookie.Value
+		}
+
+		// Add the CSRF token to the request context
+		ctx := context.WithValue(r.Context(), "csrf_token", csrfToken)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
