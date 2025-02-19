771858bfdfac16fa8bb1528362a2bb6044657564
Cliff Oyoh Omollo <oyoocliff471@gmail.com>
2025-02-19 12:12:33 +0300

    Fix: move handlers to main package to enable easy injection of dependencies defined in main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,34 +4,43 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	middleware "learn.zone01kisumu.ke/git/clomollo/forum/Middleware"
 	"learn.zone01kisumu.ke/git/clomollo/forum/internal/database"
 	"learn.zone01kisumu.ke/git/clomollo/forum/internal/handlers"
-	"learn.zone01kisumu.ke/git/clomollo/forum/internal/models"
 )
+
+// struct to hold application-wide dependencies
+type Dependencies struct {
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":8000", "HTTP network address")
 	if err := database.InitializeDB(); err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
 	}
-	// Initialize dependencies
-	deps := models.NewDependencies()
+	// leveled logging, informational messages output to standard out(stdout)
+	// Error messages output to standard error(stderr)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t",
+		log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.HomeHandler)
-	mux.Handle("/register", middleware.CSRFMiddleware(http.HandlerFunc(handlers.RegisterHandler)))
-	mux.Handle("/logout", http.HandlerFunc(handlers.LogoutHandler))
-	mux.Handle("/login", middleware.CSRFMiddleware(http.HandlerFunc(handlers.LoginHandler)))
+	mux.Handle("/register", middleware.CSRFMiddleware(http.HandlerFunc(RegisterHandler)))
+	mux.Handle("/logout", http.HandlerFunc(LogoutHandler))
+	mux.Handle("/login", middleware.CSRFMiddleware(http.HandlerFunc(LoginHandler)))
 
 	serv := &http.Server{
 		Handler:  mux,
 		Addr:     *addr,
-		ErrorLog: deps.ErrorLog,
+		ErrorLog: errorLog,
 	}
 
-	deps.InfoLog.Printf("Starting server on port %v:", *addr)
+	infoLog.Printf("Starting server on port %v:", *addr)
 	err := serv.ListenAndServe()
-	deps.ErrorLog.Fatal(err)
+	errorLog.Fatal(err)
 }
