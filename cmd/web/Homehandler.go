a777f8f9d1aa20d829c8276ce8dd54e8777b70bd
GathangaElijah <elyg3672@gmail.com>
2025-02-21 15:31:06 +0300

    refactor: Fetched the posts

--- cmd/web/Homehandler.go
+++ cmd/web/Homehandler.go
@@ -28,39 +28,18 @@ func (dep *Dependencies) HomeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Dummy Data for a post
-
-	posts := []PostView{
-		{
-			Post: models.Post{
-				PostId: "1",
-				UserId: "user123",
-				// Category:    []string{"Tech", "GoLang"},
-				Title:       "Go Template Issue",
-				PostContent: "Fixing struct and template mismatches.",
-				Media:       "https://example.com/image.jpg",
-			},
-			Comments: []string{"Great Post!", "Very helpful"},
-			Likes:    10,
-			Dislikes: 2,
-		},
-		{
-			Post: models.Post{
-				PostId: "2",
-				UserId: "user456",
-				// Category:    []string{"Programming"},
-				Title:       "Understanding Golang",
-				PostContent: "Golang is powerful for backend development.",
-				Media:       "",
-			},
-			Comments: []string{"Nice explanation", "Thanks for sharing"},
-			Likes:    15,
-			Dislikes: 0,
-		},
+	// get all the posts data
+	posts, err := dep.Forum.AllPosts()
+	if err != nil {
+		dep.ErrorLog.Println("Error Retrieving Post data\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 
+	p := PostProcessor(posts)
+
 	// Execute the template with data
-	err = tmpl.ExecuteTemplate(w, "base", posts)
+	err = tmpl.ExecuteTemplate(w, "base", p)
 	if err != nil {
 		dep.ErrorLog.Println("Error executing template:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -71,22 +50,18 @@ func (dep *Dependencies) HomeHandler(w http.ResponseWriter, r *http.Request) {
 // This function gets the likes, dislikes and comments
 // that concerns the post using the PostID
 
-/*func PostProcessor(post []models.Post) *[]PostView {
+func PostProcessor(post []models.Post) *[]PostView {
 	var postViews []PostView
 
-	for _, p := range post{
-		comments := GetComments(p.PostId)
-		likes := GetLikes(p.PostId)
-		dislikes := GetDislikes(p.PostId)
+	for _, p := range post {
+		// comments := GetComments(p.PostId)
+		// likes := GetLikes(p.PostId)
+		// dislikes := GetDislikes(p.PostId)
 
 		postViews = append(postViews, PostView{
 			Post: p,
-			Comments: comments,
-			Likes: likes,
-			Dislikes: dislikes,
 		})
 	}
 
 	return &postViews
 }
-*/
