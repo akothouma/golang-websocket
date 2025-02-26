90aa7e316cac36ed46c64c219140b6f77b02cb01
victorpaularony <smallypauls@gmail.com>
2025-02-26 03:07:57 +0300

    cmd/web/renderTemplates.go

--- cmd/web/renderTemplates.go
+++ cmd/web/renderTemplates.go
@@ -5,13 +5,16 @@ import (
 	"log"
 	"net/http"
 )
+
 var templates *template.Template
+
 // Initialize templates (call this function in your main.go or init function)
 func InitTemplates(templateDir string) {
 	templates = template.Must(template.ParseGlob(templateDir + "/*.html"))
 }
+
 // function to render the html templates pages (used for the likes and dislike form)
-//prone to change if there exists a better one
+// prone to change if there exists a better one
 func RenderTemplates(w http.ResponseWriter, fileName string, data interface{}) {
 	if err := templates.ExecuteTemplate(w, fileName, data); err != nil {
 		ErrorHandler(w, http.StatusInternalServerError)
