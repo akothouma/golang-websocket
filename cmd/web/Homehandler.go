9e3bf62bec91afb8fd2ba941e2e2bd2fe6e4626a
GathangaElijah <elyg3672@gmail.com>
2025-02-28 00:24:33 +0300

    refactor: added code to render the filtered and all posts

--- cmd/web/Homehandler.go
+++ cmd/web/Homehandler.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
 	"learn.zone01kisumu.ke/git/clomollo/forum/internal/models"
 )
@@ -15,35 +17,73 @@ type PostView struct {
 }
 
 func (dep *Dependencies) HomeHandler(w http.ResponseWriter, r *http.Request) {
+	wd, err := os.Getwd()
+	if err != nil {
+		dep.ErrorLog.Println("Error getting the working directory:\n", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// constructing the correct path
+	tmplPath := wd + "/ui/templates/"
+
 	// Parse base, home, and post templates
 	tmpl, err := template.ParseFiles(
-		"./ui/templates/base.html",
-		"./ui/templates/home.html",
-		"./ui/templates/postContent.html",
-		"./ui/templates/categories.html",
+		tmplPath+"base.html",
+		tmplPath+"home.html",
+		tmplPath+"postContent.html",
+		tmplPath+"categories.html",
 	)
 	if err != nil {
-		dep.ErrorLog.Println("Error loading template:", err)
+		dep.ErrorLog.Println("Error loading template:\n", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// get all the posts data
-	posts, err := dep.Forum.AllPosts()
+	// HANDLING THE FILTER FORM
+	// if r.Method != http.MethodPost{
+	// 	dep.ErrorLog.Println("Error during filtration of categories\n", err)
+	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	err = r.ParseForm()
 	if err != nil {
-		dep.ErrorLog.Println("Error Retrieving Post data\n", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		dep.ErrorLog.Println("Error parsing filter form:\n", err)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
+	categories := r.Form["category"]
+	//DEBUG
+	fmt.Println("This are the selected categories\n", categories)
+	if len(categories) == 0 {
+		// get all the posts data
+		posts, err := dep.Forum.AllPosts()
+		if err != nil {
+			dep.ErrorLog.Println("Error Retrieving Post data\n", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 
-	p := PostProcessor(posts)
+		// p := PostProcessor(posts)
 
-	// Execute the template with data
-	err = tmpl.ExecuteTemplate(w, "base", p)
-	if err != nil {
-		dep.ErrorLog.Println("Error executing template:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		// Execute the template with data
+		err = tmpl.ExecuteTemplate(w, "base", posts)
+		if err != nil {
+			dep.ErrorLog.Println("Error executing template:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	} else {
+		for _, cat := range categories {
+			filteredPosts, err := dep.Forum.Filters(cat)
+			if err != nil {
+				dep.ErrorLog.Println("Error Filtering the posts:", err)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+			// DEBUG
+			fmt.Println("This are the filtered posts:\n", filteredPosts)
+		}
 	}
 }
 
