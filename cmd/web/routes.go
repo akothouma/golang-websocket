1c59f05896e8749f759a81395d313282e3fe4695
GathangaElijah <elyg3672@gmail.com>
2025-03-04 09:17:53 +0300

    refactor:

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"net/http"
-
-	"learn.zone01kisumu.ke/git/clomollo/forum/internal/models"
 	// middleware "learn.zone01kisumu.ke/git/clomollo/forum/Middleware"
 )
 
@@ -16,8 +14,8 @@ func (dep *Dependencies) Routes() *http.ServeMux {
 	mux.Handle("/", dep.CSRFMiddleware(http.HandlerFunc(dep.HomeHandler)))
 	mux.Handle("/post", dep.AuthMiddleware(http.HandlerFunc(dep.PostHandler)))
 	// mux.Handle("/allposts", http.HandlerFunc(dep.AllPostsHandler))
-	mux.Handle("/allposts", http.HandlerFunc(models.RenderPostsPage))
-	
+	// mux.Handle("/allposts", http.HandlerFunc(models.RenderPostsPage))
+
 	mux.Handle("/register", dep.CSRFMiddleware(http.HandlerFunc(dep.RegisterHandler)))
 	mux.Handle("/logout", http.HandlerFunc(dep.LogoutHandler))
 	mux.Handle("/login", dep.CSRFMiddleware(http.HandlerFunc(dep.LoginHandler)))
