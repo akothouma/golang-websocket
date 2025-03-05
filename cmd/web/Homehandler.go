19b90f89384642f8a43f18dd361b322db19b604d
GathangaElijah <elyg3672@gmail.com>
2025-03-06 01:31:31 +0300

    refactor: updated the template execution

--- cmd/web/Homehandler.go
+++ cmd/web/Homehandler.go
@@ -2,58 +2,47 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
-	"os"
 
 	"learn.zone01kisumu.ke/git/clomollo/forum/internal/models"
 )
 
-type PostView struct {
-	Post     models.Post
-	Comments []string
-	Likes    int
-	Dislikes int
-}
-
 func (dep *Dependencies) HomeHandler(w http.ResponseWriter, r *http.Request) {
-	wd, err := os.Getwd()
-	if err != nil {
-		dep.ErrorLog.Println("Error getting the working directory:\n", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+	// wd, err := os.Getwd()
+	// if err != nil {
+	// 	dep.ErrorLog.Println("Error getting the working directory:\n", err)
+	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// 	return
+	// }
 
-	// constructing the correct path
-	tmplPath := wd + "/ui/templates/"
+	// // constructing the correct path
+	// tmplPath := wd + "/ui/html/"
 
-	// Parse base, home, and post templates
-	tmpl, err := template.ParseFiles(
-		tmplPath+"base.html",
-		tmplPath+"home.html",
-		tmplPath+"postContent.html",
-		tmplPath+"categories.html",
-	)
+	// // Parse base, home, and post templates
+	// tmpl, err := template.ParseFiles(
+	// 	tmplPath + "posts.html")
+	// if err != nil {
+	// 	dep.ErrorLog.Println("Error loading template:\n", err)
+	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// 	return
+	// }
+	allTemplates, err := models.InitTemplates()
 	if err != nil {
 		dep.ErrorLog.Println("Error loading template:\n", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
 	// HANDLING THE FILTER FORM
-	// if r.Method != http.MethodPost{
-	// 	dep.ErrorLog.Println("Error during filtration of categories\n", err)
-	// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	// 	return
-	// }
+	// Getting the values of the filter form
 	err = r.ParseForm()
 	if err != nil {
 		dep.ErrorLog.Println("Error parsing filter form:\n", err)
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
+	var filteredPosts interface{}
 	categories := r.Form["category"]
-	//DEBUG
+	// DEBUG
 	fmt.Println("This are the selected categories\n", categories)
 	if len(categories) == 0 {
 		// get all the posts data
@@ -64,18 +53,21 @@ func (dep *Dependencies) HomeHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		//DEBUG
+		// DEBUG
 		fmt.Println("This is the posts data >>", posts)
 		// Execute the template with data
-		err = tmpl.ExecuteTemplate(w, "base", posts)
+		
+		err = allTemplates.ExecuteTemplate(w, "base", nil)
+		err = allTemplates.ExecuteTemplate(w, "content", posts)
 		if err != nil {
 			dep.ErrorLog.Println("Error executing template:", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
+		// RenderTemplates(w, "posts.html", posts)
 	} else {
 		for _, cat := range categories {
-			filteredPosts, err := dep.Forum.Filters(cat)
+			filteredPosts, err = dep.Forum.Filters(cat)
 			if err != nil {
 				dep.ErrorLog.Println("Error Filtering the posts:", err)
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -84,24 +76,10 @@ func (dep *Dependencies) HomeHandler(w http.ResponseWriter, r *http.Request) {
 			// DEBUG
 			fmt.Println("This are the filtered posts:\n", filteredPosts)
 		}
+		allTemplates.ExecuteTemplate(w, "base", nil)
+		allTemplates.ExecuteTemplate(w, "content", filteredPosts)
 	}
 }
 
 // This function gets the likes, dislikes and comments
 // that concerns the post using the PostID
-
-func PostProcessor(post []models.Post) *[]PostView {
-	var postViews []PostView
-
-	for _, p := range post {
-		// comments := GetComments(p.PostId)
-		// likes := GetLikes(p.PostId)
-		// dislikes := GetDislikes(p.PostId)
-
-		postViews = append(postViews, PostView{
-			Post: p,
-		})
-	}
-
-	return &postViews
-}
