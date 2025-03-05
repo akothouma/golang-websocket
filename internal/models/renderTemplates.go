5628dd4bf6cbb5ebcff55dabfd1b142db1f3c709
GathangaElijah <elyg3672@gmail.com>
2025-03-05 22:19:01 +0300

    feat: initialized the templates

--- internal/models/renderTemplates.go
+++ internal/models/renderTemplates.go
@@ -1,16 +1,35 @@
 package models
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 var templates *template.Template
 
 // Initialize templates (call this function in your main.go or init function)
-func InitTemplates(templateDir string) {
-	templates = template.Must(template.ParseGlob(templateDir + "/*.html"))
+func InitTemplates() (*template.Template, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, errors.New("error getting working directory")
+	}
+	// constructing the correct path
+	tmplPath := wd + "/ui/templates/"
+	tmpl, err := template.ParseFiles(
+		tmplPath+"base.html",
+		tmplPath+"categories.html",
+		tmplPath+"home.html",
+		tmplPath+"postContent.html",
+		tmplPath+"posts.html",
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing templates: %w", err) // Wrap the error
+	}
+	return tmpl, nil
 }
 
 // function to render the html templates pages (used for the likes and dislike form)
