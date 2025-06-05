72da60e5f3853afc5fe6ebf9f82dfe912046e41c
lakoth <lornaakothpb@gmail.com>
2025-06-05 14:34:46 +0300

    refactor:reduce photo size

--- ui/static/messageComponents/imageComponent/image.js
+++ ui/static/messageComponents/imageComponent/image.js
@@ -1,8 +1,8 @@
  export default Image=()=>{
 const profile=document.createElement("img");
 profile.className='profile_image';
-profile.style.height='60px';
-profile.style.width='60px';
+profile.style.height='40px';
+profile.style.width='40px';
 profile.src="real-time-forum/ui/static/icons8-avatar-50.png"
 profile.style.borderRadius='50%';
 return profile;
